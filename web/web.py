da9cdc29b72ef394aeed8a349397d88c59e96226
Xavier Montillet <xavierm02.net@gmail.com>
2014-09-25 11:55:23 +0200

    Redirected all requests to root and added template.

--- web/web.py
+++ web/web.py
@@ -1,10 +1,20 @@
 import webapp2
+import os
+from google.appengine.ext.webapp import template
 
-class MainPage(webapp2.RequestHandler):
+class Redirect(webapp2.RequestHandler):
+	def get(self):
+		return webapp2.redirect('/')
+
+class Main(webapp2.RequestHandler):
     def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Hello, World!')
+		template_values = {
+            'url':'a',
+            'url_linktext': 'b',
+        }
+		self.response.out.write(template.render('index.html', template_values))
 
 application = webapp2.WSGIApplication([
-    ('/', MainPage),
+	('/', Main),
+    ('/.*', Redirect)
 ], debug=True)
